23ed480779dd136269dc20fe613a1ccd6d8d0ae6
ray <romerxzh@gmail.com>
2019-05-03 13:41:32 +0800

    Init the login

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    this.globalData = {}
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -9,8 +9,16 @@ App({
         env: 'junye-sign-0bhda',
         traceUser: true,
       })
+      wx.cloud.callFunction(
+        {
+          name : "login",
+          data : {},
+          success: res=>{
+            console.log("login调用成功" +res.result.openid),
+            this.globalData.openid=res.result.openid
+          }
+        }
+      )
     }
-
-    this.globalData = {}
   }
 })
