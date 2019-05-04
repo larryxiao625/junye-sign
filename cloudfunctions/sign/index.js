058cab9a42a48eea43e99f9784b6334a0cfbd7e9
ray <romerxzh@gmail.com>
2019-05-04 14:04:25 +0800

    Add the check distance

--- cloudfunctions/sign/index.js
+++ cloudfunctions/sign/index.js
@@ -5,6 +5,41 @@ cloud.init()
 const db = cloud.database()
 const _ = db.command
 // 云函数入口函数
+function isInArea(latitudePeople, longitudePeople, latitudeLocation, longitudeLocation) {
+  console.log(latitudePeople+" "+longitudePeople);
+  console.log(latitudeLocation+" "+longitudeLocation);
+  var f = rad((latitudePeople + latitudeLocation) / 2);
+  var g = rad((latitudePeople - latitudeLocation) / 2);
+  var l = rad((longitudePeople - longitudeLocation) / 2);
+  var sg = Math.sin(g);
+  var sl = Math.sin(l);
+  var sf = Math.sin(f);
+  var s, c, w, r, d, h1, h2;
+  var a = 6378137.0;//The Radius of eath in meter.
+  var fl = 1 / 298.257;
+  sg = sg * sg;
+  sl = sl * sl;
+  sf = sf * sf;
+  s = sg * (1 - sl) + (1 - sf) * sl;
+  c = (1 - sg) * (1 - sl) + sf * sl;
+  w = Math.atan(Math.sqrt(s / c));
+  r = Math.sqrt(s * c) / w;
+  d = 2 * w * a;
+  h1 = (3 * r - 1) / 2 / c;
+  h2 = (3 * r + 1) / 2 / s;
+  s = d * (1 + fl * (h1 * sf * (1 - sg) - h2 * (1 - sf) * sg));
+  console.log(s);
+  if(s>150){
+    return false;
+  }else{
+     return true;
+  }
+}
+
+function rad(d) {
+  var PI=Math.PI;
+  return d*PI/180.0;
+}
 exports.main = async(event, context) => new Promise((resolve, reject) => {
   var date = new Date()
   const {
@@ -25,29 +60,33 @@ exports.main = async(event, context) => new Promise((resolve, reject) => {
       db.collection('signHistory').where({
         signActivityId: event.activityId,
         signStudentId: OPENID
-      }).get().then(res=>{
+      }).get().then(res => {
         console.log(res.data);
         if (res.data.length > 0) {
           resolve("已经签到");
         } else {
-          db.collection('signHistory').add({
-            data: {
-              signTime: date,
-              signActivityId: activity._id,
-              signStudentId: OPENID
-            }
-          }).then(function (res) {
-            console.log(res);
-            resolve("签到成功");
-          }).catch(console.error)
+          if (isInArea(event.latitude, event.longitude, activity.coordinate.latitude, activity.coordinate.longitude)) {
+            db.collection('signHistory').add({
+              data: {
+                signTime: date,
+                signActivityId: activity._id,
+                signStudentId: OPENID
+              }
+            }).then(function(res) {
+              console.log(res);
+              resolve("签到成功");
+            }).catch(console.error)
+          } else {
+            reject("未到指定区域");
+          }
         }
-      }).catch(res=>{
+      }).catch(res => {
         console.log("queryError");
       })
     } else {
       reject("未到签到时间");
     }
-  }).catch(res=>{
+  }).catch(res => {
     console.log("fail");
     reject("签到失败")
   })
