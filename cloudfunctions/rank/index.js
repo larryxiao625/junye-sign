1fffa14d229c43b4a43efaf34a8ce6ae189df763
ray <romerxzh@gmail.com>
2019-05-16 09:16:30 +0800

    Fix rank while not useable

--- cloudfunctions/rank/index.js
+++ cloudfunctions/rank/index.js
@@ -15,22 +15,15 @@ var rank = new Array();
 exports.main = async(event, context) => {
   date = new Date(new Date().getTime() + 28800000);
   const wxContext = cloud.getWXContext()
+  var tempRank;
   if (event.type == "周排名") {
     getWeekTime();
     var lastMondayDate = new Date(last_monday);
     let res = await db.collection("signHistory").where({
       signTime: _.gt(lastMondayDate).and(_.lt(date))
-    }).orderBy('signStudentId','asc').get().then(res => {
-      console.log(res);
-      console.log("success");
-      rankThePeople(res);
-      console.log(rank);
-    }).catch(err => {
-      console.log('Error:', err);
-      console.log('111111111111111111111111');
-      return err;
-    });
-    return rank;
+    }).orderBy('signStudentId','asc').get();
+    console.log(res);
+    return rankThePeople(res);
   }
   if (event.type == "月排名") {
     console.log(date);
@@ -40,17 +33,10 @@ exports.main = async(event, context) => {
     console.log(lastMondayDate);
     let res = await db.collection("signHistory").where({
       signTime: _.gt(lastMondayDate).and(_.lt(date))
-    }).orderBy('signStudentId', 'asc').get().then(res => {
-      console.log(res);
-      console.log("success");
-      rankThePeople(res);
-      console.log(rank);
-    }).catch(err => {
-      console.log('Error:', err);
-      return err;
-    });
+    }).orderBy('signStudentId', 'asc').get();
+    console.log(res);
+    return rankThePeople(res);
   }
-  return rank;
 }
 
 function getWeekTime() {
@@ -69,23 +55,38 @@ async function rankThePeople(res){
   var count=0;
   var id;
   var number=0;
+  var name;
   for(var i=0;i<res.data.length;i++){
     console.log(res.data[i].signStudentId);
-    var name;
-    await db.collection('student').where({
-      stuOpenId: res.data[i].signStudentId
-    }).get().then(res=>{
-      name=res.stuName;
-    });
-    console.log(name);
+    console.log(i);
     if(i===0){
+      try{
+        let tempName = await db.collection('student').where({
+          stuOpenId: res.data[i].signStudentId
+        }).get();
+        name = tempName.data[0].stuName;
+      }catch(err){
+        console.log(err);
+      }
+      console.log(name);
+      console.log("i==0");
       id=res.data[i].signStudentId;
       count++;
       continue;
     }
     if(res.data[i].signStudentId!=id){
+      try{
+        let tempName = await db.collection('student').where({
+          stuOpenId: res.data[i].signStudentId
+        }).get();
+        name=tempName.data[0].stuName;
+      }catch(err){
+        console.log(err);
+      }
+      console.log(name);
+      console.log("!");
       var json={
-        "stuId": res.data[i].signStudentId,
+        "stuId": name,
         "count": count
       }
       rank.push(json);
@@ -95,9 +96,10 @@ async function rankThePeople(res){
       continue;
     }
     if(i==res.data.length-1){
+      console.log("last");
       count++;
       var json = {
-        "stuId": res.data[i].signStudentId,
+        "stuId": name,
         "count": count
       }
       rank.push(json);
@@ -106,4 +108,5 @@ async function rankThePeople(res){
     }
     count++;
   }
+  return rank;
 }
